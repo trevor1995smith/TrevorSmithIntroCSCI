82a8653da1939aed50feccd263f6307e2fe17765
trevor1995smith <backcountryhnter@gmail.com>
2025-04-03 13:45:46 -0600

    Assignment 10

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 #Trevor Smith
-#Assignment 9
-#3/23/25
+#Assignment 10
+#4/3/25
 
 
 # Import the gamefunctions module
@@ -13,13 +13,15 @@ def main():
     """
     user_hp = 30
     user_gold = 10
+    inventory = []
+    equipped_weapon = None  # Initialize as None at the global level
 
     print("Welcome to the game!")
     user_name = input("Please enter your name: ")
     gamefunctions.print_welcome(user_name)
 
     while True:
-        gamefunctions.display_town_menu(user_hp, user_gold)
+        gamefunctions.display_town_menu(user_hp, user_gold, equipped_weapon)
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -27,6 +29,10 @@ def main():
         elif choice == "2":
             user_hp, user_gold = gamefunctions.sleep(user_hp, user_gold)
         elif choice == "3":
+            user_gold, inventory = gamefunctions.visit_shop(user_gold, inventory)
+        elif choice == "4":
+            equipped_weapon, user_hp = gamefunctions.manage_inventory(equipped_weapon, user_hp, inventory)
+        elif choice == "5":
             print("\nThanks for playing! Goodbye!")
             break
         else:
